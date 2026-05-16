8681509389d8d82ceb03c3bfb752fccdfb41d952
iririn1998 <iririn199810710@gmail.com>
2026-05-16 21:30:01 +0900

    遅延つける

--- src/routes/api.hello.ts
+++ src/routes/api.hello.ts
@@ -3,10 +3,12 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/api/hello')({
   server: {
     handlers: {
-      GET: async () =>
-        new Response('Hello World', {
+      GET: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        return new Response('Hello World', {
           headers: { 'Content-Type': 'text/plain' },
-        }),
+        })
+      },
     },
   },
 })
